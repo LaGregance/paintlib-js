4743c235bfd029288fef3d8671697948c32b8fed
LaGregance <gregoire.taja@gmail.com>
2024-12-16 11:08:32 +0100

    Force fontFamily to Times New Roman

--- src/paintlib/objects/paint-text.ts
+++ src/paintlib/objects/paint-text.ts
@@ -14,7 +14,7 @@ export class PaintText extends PaintObject<Textbox> {
       left: point.x,
       objectCaching: false,
       fontSize: extras?.fontSize ?? 5,
-      // fontFamily: 'Times New Roman',
+      fontFamily: 'Times New Roman',
     });
 
     this.fabricObject.editable = true;
